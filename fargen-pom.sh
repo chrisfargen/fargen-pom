745d62bf53d181e9d2c30d51ce93c1b9c80c264e
Christopher Fargen <chrisfargen@gmail.com>
2013-11-16 21:09:08 -0500

    Cleaned up files

--- fargen-pom.sh
+++ fargen-pom.sh
@@ -4,7 +4,6 @@
 pomodoro_timer=25
 break_timer=5
 set_timer=30
-time_up_cmd=$(echo "Time is up!")
 
 if [ -z "$1" ]
 then
@@ -14,26 +13,25 @@ elif [ "$1" = "start" ]
 then
     clear
     echo "** Starting..."
-    echo "** You have $pomodoro_timer minutes." 
-    echo "** What is your goal?"
-    read -p "** " goal
+    echo "** T: $pomodoro_timer minutes" 
+    read -p "** G: " goal
 
     start_unix=$(date -d "+ 1 minute")
     start_str=$(date +%F\ %R -d "$start_unix")
     end_str=$(date +%F\ %R -d "$start_unix + $pomodoro_timer minutes")
-    echo -e "** START:\t$start_str"
-    echo -e "** END:  \t$end_str"
+    echo "** S: $start_str"
+    echo "** E: $end_str"
 
     # SOUND
     #http://www.unix.com/showpost.php?s=9bc2b8e5a791d32d259d221a61811f14&p=302358739&postcount=6
     (for (( i=$pomodoro_timer; i>0; i--)); do
         sleep 1m &
-        printf "** TO GO:\t$i min \r"
+        printf "** R: $i minutes\r"
         #play -qn synth 0.01 noise A vol 0.01 &
         wait
     done) 
     
-    #echo "** TIME IS UP!"
+    #echo "** Time is up!"
     play -q /var/www/fargen-pom/bell.wav vol 0.25
 
     INIT=""
@@ -43,23 +41,24 @@ then
     
     if [ $exitstatus = 0 ]
     then
-        echo -e "** DONE:\t$accomplishment"
+        echo "** D: $accomplishment"
     else
-        echo "** POMODORO CANCELED!"
+        echo "** Pomodoro canceled."
+        exit 0
     fi
      
     echo "** Adding entry to sqlite..."
     # Quick and dirty insert.
-    sqlite3 fargenpom <<-EOM
+    sqlite3 fargenpomdb <<-EOM
 CREATE TABLE IF NOT EXISTS pomodoro(start datetime, end datetime, goal text, accomplishment text, submitted datetime default current_timestamp);
 INSERT INTO pomodoro (start, end, goal, accomplishment) VALUES('$start_str','$end_str','$goal','$accomplishment');
 EOM
 
-    echo "Hurray!"
+    echo "** Hurray!"
 
 elif [ "$1" = "ls" ]
 then
-    sqlite3 fargenpom <<-EOM
+    sqlite3 fargenpomdb <<-EOM
 .mode column
 .headers on
 SELECT * FROM pomodoro;
